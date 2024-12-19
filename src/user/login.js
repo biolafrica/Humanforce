0f9d84f326ff7464e162b451c7d43843f612c4d7
Abiodun Biobaku <biolafrica@gmail.com>
2024-12-19 12:56:53 +0000

    setup login

--- src/user/login.js
+++ src/user/login.js
@@ -1,18 +1,63 @@
+import { useState } from "react";
+import useForm from "../hooks/useForm";
+import axios from "axios";
+import {useNavigate } from "react-router-dom";
+
 const Login = ()=>{
+  const navigate = useNavigate()
+
+  const initialValue = ({
+    staff_code: ""
+  })
+
+  const{formData, handleInputChange, resetForm} = useForm(initialValue)
+
+  const handleSubmit = async(e)=>{
+    e.preventDefault();
+    try {
+      const response = await axios.post("http://localhost:4000/login", formData);
+      const data = response.data;
+      const {token} = response.data;
+
+      localStorage.setItem("authToken", token)
+
+      if(data){
+        navigate("/");
+      }
+      alert("Logged in succesfully");
+      resetForm();
+      
+    } catch (error) {
+      console.log("Login failed;", error);
+      alert("Invalid login credentials");
+      
+    }
+  }
 
   return(
     <div className="login_container">
       <h2>Sign In</h2>
 
-      <label htmlFor=""><h4>Staff Number:</h4></label>
-      <input type="text" placeholder="Enter your staff number"/>
-      <div className="error_message"></div>
+      <form action="" onSubmit={handleSubmit}>
 
-      <button className="filled-btn"><h4>Sign In</h4></button>
-      
+        <label htmlFor="staff_code"><h4>Staff Code:</h4></label>
+        <input 
+          type="text" 
+          placeholder="Enter your staff number"
+          name="staff_code"
+          value={formData.staff_code}
+          onChange={handleInputChange}
+        />
+        <div className="error_message"></div>
+
+        <button className="filled-btn" type="submit"><h4>Sign In</h4></button>
+
+      </form>
+
+    
     </div>
   )
-
+ 
 }
 
 
