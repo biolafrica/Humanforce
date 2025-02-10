933bae64f633aac2e28ce7726ed6948a1847c58a
Abiodun Biobaku <biolafrica@gmail.com>
2025-02-10 14:02:48 +0000

    sorted axios error

---
+++ src/test/Pages/user/clockDetails.test.js
@@ -0,0 +1,13 @@
+jest.mock('axios');
+
+const axios = ("axios")
+const ClockDetails = ("../../../pages/user/clockDetails");
+const { render } = require("@testing-library/react");
+const UseFetch = require("../../../hooks/userFetch")
+
+
+
+describe("test suites: test clockDetails components", ()=>{
+
+  test("test case", async()=>{})
+})
